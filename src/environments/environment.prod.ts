53e09c6eb4add056d40b6dc16443320a69095941
Taiwo Sunday <taiwosunday99@gmail.com>
2019-11-01 08:39:30 +0100

    push tp prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDRxjLtSY7S24tZzz-WVMIrXnDaFBazkn4",
-    authDomain: "satirerecordsacademy-8adb0.firebaseapp.com",
-    databaseURL: "https://satirerecordsacademy-8adb0.firebaseio.com",
-    projectId: "satirerecordsacademy-8adb0",
-    storageBucket: "satirerecordsacademy-8adb0.appspot.com",
-    messagingSenderId: "750050761833",
-    appId: "1:750050761833:web:a8da637db33b0bf6e796f8",
-    measurementId: "G-XZRDQRRTL4"
+   firebaseConfig: {
+    apiKey: "AIzaSyAlVjjuzwPrT-GZr79cGtoclXa6Cg_2I6c",
+    authDomain: "satirerecordsacademy100.firebaseapp.com",
+    databaseURL: "https://satirerecordsacademy100.firebaseio.com",
+    projectId: "satirerecordsacademy100",
+    storageBucket: "satirerecordsacademy100.appspot.com",
+    messagingSenderId: "744384118744",
+    appId: "1:744384118744:web:7f6182afa907beb6affcad"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,39 @@
 export const environment = {
   production: false,
    firebaseConfig: {
-    apiKey: "AIzaSyDRxjLtSY7S24tZzz-WVMIrXnDaFBazkn4",
-    authDomain: "satirerecordsacademy-8adb0.firebaseapp.com",
-    databaseURL: "https://satirerecordsacademy-8adb0.firebaseio.com",
-    projectId: "satirerecordsacademy-8adb0",
-    storageBucket: "satirerecordsacademy-8adb0.appspot.com",
-    messagingSenderId: "750050761833",
-    appId: "1:750050761833:web:a8da637db33b0bf6e796f8",
-    measurementId: "G-XZRDQRRTL4"
+    apiKey: "AIzaSyAlVjjuzwPrT-GZr79cGtoclXa6Cg_2I6c",
+    authDomain: "satirerecordsacademy100.firebaseapp.com",
+    databaseURL: "https://satirerecordsacademy100.firebaseio.com",
+    projectId: "satirerecordsacademy100",
+    storageBucket: "satirerecordsacademy100.appspot.com",
+    messagingSenderId: "744384118744",
+    appId: "1:744384118744:web:7f6182afa907beb6affcad"
   }
 
 };
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAlVjjuzwPrT-GZr79cGtoclXa6Cg_2I6c",
+//     authDomain: "satirerecordsacademy100.firebaseapp.com",
+//     databaseURL: "https://satirerecordsacademy100.firebaseio.com",
+//     projectId: "satirerecordsacademy100",
+//     storageBucket: "satirerecordsacademy100.appspot.com",
+//     messagingSenderId: "744384118744",
+//     appId: "1:744384118744:web:7f6182afa907beb6affcad"
+//   };
+
+
+
+  //    firebaseConfig: {
+  //   apiKey: "AIzaSyDRxjLtSY7S24tZzz-WVMIrXnDaFBazkn4",
+  //   authDomain: "satirerecordsacademy-8adb0.firebaseapp.com",
+  //   databaseURL: "https://satirerecordsacademy-8adb0.firebaseio.com",
+  //   projectId: "satirerecordsacademy-8adb0",
+  //   storageBucket: "satirerecordsacademy-8adb0.appspot.com",
+  //   messagingSenderId: "750050761833",
+  //   appId: "1:750050761833:web:a8da637db33b0bf6e796f8",
+  //   measurementId: "G-XZRDQRRTL4"
+  // }
 
 /*
  * For easier debugging in development mode, you can import the following file
